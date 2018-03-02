64a343268f99f8383dd5b380a678a98b9e03139e
NikaMakovik <nika.makovik@gmail.com>
2018-03-02 23:02:10 +0300

    some fixes

---
+++ DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DEV_1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Write("Wrong number of arguments.");
+            }
+            else
+            {
+                SerialIdenticalSymbols serialIdenticalSymbols = new SerialIdenticalSymbols();
+                int MaxNumberOfSerialIdenticalSymbols = serialIdenticalSymbols.GetMaxNumberOfSerialIdenticalSymbols(args[0]);
+                Console.WriteLine("Max value of serial identical symbols is: " + MaxNumberOfSerialIdenticalSymbols);
+            }
+        }
+    }
+}
--- DEV-1/DEV-1/SerialIdenticalSymbols.cs
+++ DEV-1/DEV-1/SerialIdenticalSymbols.cs
@@ -8,24 +8,28 @@
         /// <summary>
         /// This class calculates number of serial identical symbols.
         /// </summary>
-        /// <param name="theString"></param>
+        /// <param name="theString">The main string.</param>
         /// <returns>
         /// Returns maximum number of serial identical symbols.
         /// </returns>
-        static public int GetMaxNumberOfSerialIdenticalSymbols(string theString)
+        public int GetMaxNumberOfSerialIdenticalSymbols(string theString)
         {
             int counter = 1;
             int maxEquals = 1;
             for (int i = 0; i < theString.Length - 1; i++)
-            { 
+            {
                 if (theString[i + 1] == theString[i])
                 {
                     counter++;
                     if (counter > maxEquals)
+                    {
                         maxEquals = counter;
+                    }
                 }
                 else
+                {
                     counter = 1;
+                }
             }
             return maxEquals;
         }
