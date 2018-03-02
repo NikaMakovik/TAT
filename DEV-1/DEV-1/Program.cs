9925e6c3e4a4508246e167f757dedf3a62c3e988
NikaMakovik <nika.makovik@gmail.com>
2018-03-02 18:14:14 +0300

    task_DEV-1

---
+++ DEV-1/DEV-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DEV_1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Write("Wrong number of arguments.");
+            }
+            else
+            {
+                string theString = args[0];
+                Console.WriteLine($"Max value of serial identical symbols is: {DEV_1.SerialIdenticalSymbols.GetMaxNumberOfSerialIdenticalSymbols(theString)}");
+            }
+        }
+    }
+}
