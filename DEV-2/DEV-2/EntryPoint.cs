0e3a3b1667039214285b31a8294ad62515bb6329
NikaMakovik <nika.makovik@gmail.com>
2018-03-04 17:35:02 +0300

    add task DEV-2

---
+++ DEV-2/DEV-2/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your string: ");
+            StringBuilder theString = new StringBuilder(Console.ReadLine());
+            StringCreator newString = new StringCreator();
+            newString.GetNewStringOddIndices(theString);
+        }
+    }
+}
