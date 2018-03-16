d5072f7652b2d39e380187f9762ce7721dcefada
NikaMakovik <nika.makovik@gmail.com>
2018-03-16 18:14:08 +0300

    codestyle fixes

--- DEV-3/UnitTestProject2/NumberConverterTest.cs
+++ DEV-3/UnitTestProject2/NumberConverterTest.cs
@@ -7,13 +7,13 @@ namespace DEV_3.Tests
     [TestFicture]
     public class NumberConverterTest
     {
-        [TestCase(255, 16, "FF")]
-        [TestCase(10, 2, "1010")]
-        [TestCase(20, 20, "10")]
+        [TestCase(1000, 16, "3E8")]
+        [TestCase(8, 2, "1000")]
+        [TestCase(5, 17, "5")]
         [TestCase(Int32.MaxValue, 20, "1DB1F927")]
-        [TestCase(0, 10, "0")]
+        [TestCase(0, 16, "0")]
         [Test]
-        public void ConvertToNewNumeralSystem_(int accepted, int exception, string result)
+        public void ConvertToNewNumeralSystem_Value_ExpectCorrectConvert(int accepted, int exception, string result)
         {
             NumberConverter numberConverter = new NumberConverter(accepted, exception);
             StringBuilder actual = numberConverter.ConvertToNewNumeralSystem();
@@ -24,7 +24,7 @@ namespace DEV_3.Tests
         [TestCase(1, 21)]
         [TestCase(1, 1)]
         [Test]
-        public void CreatingRepresentationOfNumberInNewSystemTest_ArgumentOutOfRangeException(int acceptedNumber, int ecceptedFoundetion)
+        public void ConvertToNewNumeralSystem_NotValidNumbers_ExpectExceptions(int acceptedNumber, int ecceptedFoundetion)
         {
             NumberConverter numberConverter = new NumberConverter(acceptedNumber, ecceptedFoundetion);
             Assert.Throws<Exception>
