5b8443bfae25fcd0fb341912c439e0d92466e44f
NikaMakovik <nika.makovik@gmail.com>
2018-05-10 16:10:35 +0300

    DEV-10

---
+++ DEV-10/DEV-10/BrandPage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_10
+{
+    class BrandsPage
+    {
+        private readonly By ShowAllBrandsButton = By.XPath("//input[@id='model_id']");
+        private readonly By Brands = By.XPath("ul[@class='brandsitem brandsitem--primary']");
+        private readonly By Message = By.XPath($"//span[contains(text(),'{}')]");
+        private IWebDriver webDriver;
+        public List<string> listOfBrands;
+
+        public BrandsPage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public BrandsPage GetListOfBrands()
+        {
+            webDriver.FindElement(ShowAllBrandsButton).Click();
+            ReadOnlyCollection<IWebElement> brands = webDriver.FindElements(Brands);
+            listOfBrands = new List<string>();
+            foreach (IWebElement element in brands)
+            {
+                listOfBrands.Add(element.Text);
+            }
+            return this;
+        }
+        public BrandsPage GoToBrand(string brand)
+        {
+            
+        webDriver.FindElement("").Click();
+        }
+    }
+}
---
+++ DEV-10/DEV-10/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEV_10
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            AvBy avby = new AvBy();
+            avby.CarFinder();
+        }
+    }
+}
